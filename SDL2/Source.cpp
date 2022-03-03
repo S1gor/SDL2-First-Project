1b6ef98004eb69a50b265467e063779a3e8f2b6a
Igor Semikin <i.semikin@list.ru>
2022-03-03 22:18:06 +0300

    Убрал лишнии действия

--- SDL2/Source.cpp
+++ SDL2/Source.cpp
@@ -47,10 +47,18 @@ void Init()
 	}
 }
 
-void mathCoordsToScreen(double x, double y, double scale, int centerx, int centery, int& sx, int& sy)
+SDL_Texture* loadTextyreFromFile(const char* filename, SDL_Rect* rect)
 {
-	sx = round(centerx + x * scale);
-	sy = round(centery - y * scale);
+	SDL_Surface* surface = IMG_Load(filename);
+	if (surface == NULL)
+	{
+		printf("Не удалось загрузить картинку!");
+		DeInit(1);
+	}
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(ren, surface);
+	*rect = { 0,0,surface->w,surface->h };
+	SDL_FreeSurface(surface);
+	return texture;
 }
 
 int main(int args, char* argv[])
@@ -58,32 +66,26 @@ int main(int args, char* argv[])
 	system("chcp 1251"); system("cls");
 	Init();
 
-	SDL_Surface* surface = IMG_Load("diablo.jpg");
-	if (surface == NULL)
-	{
-		printf("Не удалось загрузить картинку!");
-		DeInit(1);
-	}
-	SDL_Texture* texture = SDL_CreateTextureFromSurface(ren, surface);
-	SDL_FreeSurface(surface);
+	SDL_Rect diablo_rect;
+	SDL_Texture* diablo_tex = loadTextyreFromFile("diablo.jpg", &diablo_rect);
 
-	SDL_SetRenderDrawColor(ren, 205, 205, 205, 255);
-	SDL_RenderClear(ren);
-	
-	double scale = 1.5;
-	double x1, y1, x2, y2;
-	int sx1, sy1, sx2, sy2;
-	
-	int want_point = 3;
-	bool rising = true;
 
 	SDL_Event ev;
 	bool isRunning = true;
+
 	int mousex = win_width / 2; int mousey = win_height / 2;
 	bool isUpPressed = false; bool isDownPressed = false; bool isRightPressed = false; bool isLeftPressed = false;
 
 	while (isRunning)
 	{
+		#pragma region DRAWING
+		SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+		SDL_RenderClear(ren);
+
+		SDL_RenderCopy(ren, diablo_tex, &diablo_rect, NULL);
+		#pragma endregion
+
+
 		while (SDL_PollEvent(&ev))
 		{
 			switch (ev.type)
@@ -142,57 +144,12 @@ int main(int args, char* argv[])
 		if (isRightPressed && !isLeftPressed)	mousex += 5;
 		if (!isRightPressed && isLeftPressed)	mousex -= 5;
 
-		#pragma region DRAWING CIRCLE
-		SDL_SetRenderDrawColor(ren, 205, 205, 205, 255);
-		SDL_RenderClear(ren);
-
-		SDL_SetRenderDrawColor(ren, 0, 0, 255, 255);
-		x1 = -175, x2 = 175;
-		y1 = 0, y2 = 0;
-
-		mathCoordsToScreen(x1, y1, scale, mousex, mousey, sx1, sy1);
-		mathCoordsToScreen(x2, y2, scale, mousex, mousey, sx2, sy2);
-		SDL_RenderDrawLine(ren, sx1, sy1, sx2, sy2);
-
-		y1 = -175, y2 = 175;
-		x1 = 0, x2 = 0;
-
-		mathCoordsToScreen(x1, y1, scale, mousex, mousey, sx1, sy1);
-		mathCoordsToScreen(x2, y2, scale, mousex, mousey, sx2, sy2);
-		SDL_RenderDrawLine(ren, sx1, sy1, sx2, sy2);
-
-		int point_count = want_point;
-		SDL_Point* points = (SDL_Point*)malloc(sizeof(SDL_Point) * (point_count + 1));
-
-		float alpha = 0;
-		for (int i = 0; i < point_count; i++)
-		{
-			alpha += 2 * M_PI / point_count;
-			mathCoordsToScreen(200 * cos(alpha), 200 * sin(alpha), 1.0, mousex, mousey, points[i].x, points[i].y);
-		}
-		points[point_count] = points[0];
-		
-		SDL_SetRenderDrawColor(ren, 255, 0, 0, 255);
-		SDL_RenderDrawLines(ren, points, point_count + 1);
-
-		free(points);
-
-		if (rising)
-			want_point++;
-		else
-			want_point--;
-
-		if (rising && want_point > 30 || !rising && want_point <= 3)
-			rising = !rising;
-		#pragma endregion
-
-		SDL_RenderCopy(ren, texture, NULL, NULL);
 
 		SDL_RenderPresent(ren);
 		SDL_Delay(50);
 	}
 	
-	SDL_DestroyTexture(texture);
+	SDL_DestroyTexture(diablo_tex);
 
 	DeInit(0);
 	return 0;
